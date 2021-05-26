608b98cdbd0eac5a2499ad19b428435eecca8974
unknown <kai.wolff.92@gmail.com>
2021-05-26 14:39:19 +0100

    README and .gitignore added

---
+++ python_api.py
@@ -0,0 +1,4 @@
+#Use python to make an API call using a package called requests
+#dependency is pip (package manager). Use pip to install requests using 'pip install requests'
+#The above is required for every new project, as it needs to be installed in every new virtual environment
+import requests
